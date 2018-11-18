2be0d100d837999feabd9780db16e33fb8fc8476
smashiach <44545319+smashiach@users.noreply.github.com>
2018-11-18 19:32:33 +0200

    Add files via upload

--- face_extract.py
+++ face_extract.py
@@ -1,40 +1,35 @@
-from random import randint
 import cv2
 import sys
-import os
 import numpy as np
 
-CASCADE = "haarcascade_frontalface_default.xml"
+CASCADE = "D:\\Shahar\\Project A\\part1\\haarcascade_frontalface_default.xml"
 FACE_CASCADE = cv2.CascadeClassifier(CASCADE)
 
 
-def detect_faces(image_path):
+def detect_faces(image_path,sample_delta):
+    # first part : detect face from image and crop the detected face to new file
     image = cv2.imread(image_path)
-    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    faces = FACE_CASCADE.detectMultiScale(image_grey, scaleFactor=1.1, minNeighbors=5, minSize=(25, 25), flags=0)
-
-    for x, y, w, h in faces:
+    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converting image to grey colors
+    faces = FACE_CASCADE.detectMultiScale(image_grey, scaleFactor=1.1, minNeighbors=5, minSize=(25, 25), flags=0)  # find face by haarcascade algorithm
+    for x, y, w, h in faces:  # crop face from image and convert colors to HSV scale
         sub_img = image[y - 10:y + h + 10, x - 10:x + w + 10]
-        #os.chdir("Extracted")
-        sub_image_grey = cv2.cvtColor(sub_img, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("extract_" + str(sys.argv[1]), sub_image_grey)
-        print(np.mean(sub_image_grey))
-        #os.chdir("../")
-        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        sub_image_hsv = cv2.cvtColor(sub_img, cv2.COLOR_BGR2HSV) # convert the face picture to HSV
+        #cv2.imwrite("extract_" + str(sys.argv[1]), sub_image_hsv)
 
-    cv2.imshow("Faces Found", image)
-    if (cv2.waitKey(0) & 0xFF == ord('q')) or (cv2.waitKey(0) & 0xFF == ord('Q')):
-        cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-
-    #if not "Extracted" in os.listdir("."):
-        #os.mkdir("Extracted")
-
-    if len(sys.argv) < 2:
-        print("Usage: python Detect_face.py 'image path'")
-        sys.exit()
-
-    detect_faces(sys.argv[1])
+    # second part : calculate average colors from the face
+    number_of_iteration = 100  # average of 100 iterations for each face
+    tmp_sum = 0
+    height, width = sub_image_hsv.shape[:2]  # extract image dimensions
+    mu_x = round(width/2)
+    mu_y = round(height/2)
+    sigma_x = 1
+    sigma_y = 1
+    for i in range(1,number_of_iteration+1):  # calculate random pixel by gaussian segmentation
+        x = np.random.normal(mu_x, sigma_x, 1) # calculate a random x_pixel
+        y = np.random.normal(mu_y, sigma_y, 1) # calculate a random y_pixel
+        pixel_x = int(np.round(x))
+        pixel_y = int(np.round(y))
+        avg = np.average(sub_image_hsv[pixel_x-sample_delta:pixel_x + sample_delta, pixel_y-sample_delta:pixel_y + sample_delta]) # calculate the average of rectangle
+        tmp_sum = tmp_sum + avg
+    ans = tmp_sum/number_of_iteration
+    return ans
---
+++ find_similar_face_color.py
@@ -0,0 +1,33 @@
+from face_extract import detect_faces
+import glob
+import numpy
+
+sample_delta = 30
+avg_color_pic_arr = []
+potential_pic_array = []
+
+
+for img in glob.glob('D:\\Shahar\\Project A\\compare\\training_set\\*.jpg'): #The location of training set
+    ans = detect_faces(img, sample_delta)
+    name = img.split("\\")[5] #Get the picture name
+    #print('The avarage of pic',name, 'is:', ans)
+    avg_color_pic_arr.append(ans)
+
+avg = numpy.average(avg_color_pic_arr) #Get the avarage of all the training pictures
+std = numpy.std(avg_color_pic_arr) #Get the std of all the training pictures
+
+#print('The avarage is:', avg)
+#print('The std is:', std)
+#print('The range is from', avg-std, 'to',avg+std)
+
+for pic in glob.glob('D:\\Shahar\\Project A\\compare\\test_set\\*.jpg'): #The location of testing set
+    pic_avg = detect_faces(pic, sample_delta)
+    name2 = pic.split("\\")[5] #Get the picture name
+    #print('The avarage of pic',name2, 'is:', pic_avg)
+    if (pic_avg >= avg-std) and (pic_avg <= avg+std): #Add to array only the relevant pictures
+        potential_pic_array.append(pic)
+
+print(potential_pic_array)
+
+
+
